71a35a5bc1786e2df944fe6a1bbabd3a8e70f718
Tomas Homola <homola.tom6@gmail.com>
2020-09-25 23:31:59 +0200

    Alpha verzia sposobu nacitania dat z txt suboru

--- cv1.c
+++ cv1.c
@@ -18,3 +18,42 @@ typedef struct
 	float rozpocet;
 	PRODUKT kupene_produkty[50];
 } ZAKAZNIK;
+
+int main()
+{
+	FILE *subor;
+	PRODUKT produkt[10];
+	int ID;
+	char nazov[20];
+	char vyrobca[20];
+	int pocet_kusov;
+	float cena;
+	int i, pocet_produktov;
+
+	subor = fopen("produkty.txt", "r");
+	if (subor == NULL)
+		printf("zle otvoreny subor\n");
+
+	fscanf(subor, "%d", &pocet_produktov);
+
+	for (i = 0; i < pocet_produktov; i++)
+	{
+	fscanf(subor, "%d %s %s %d %f", &ID, nazov, vyrobca, &pocet_kusov, &cena);
+
+	produkt[i].ID = ID;
+	strcpy(produkt[i].nazov, nazov);
+	strcpy(produkt[i].vyrobca, vyrobca);
+	produkt[i].pocet_kusov = pocet_kusov;
+	produkt[i].cena = cena;
+
+	printf("Produkt ID: %d, nazov: %s, vyrobca: %s, pocet kusov: %d, cena: %.2f\n", produkt[i].ID, produkt[i].nazov, produkt[i].vyrobca,
+	produkt[i].pocet_kusov, produkt[i].cena);
+	}
+
+
+
+
+
+
+	return 0;
+}
